9f068f88da594b169c532cc96acb8abee9e167ad
unknown <shuklasaurabh534@gmail.com>
2024-04-12 16:49:22 +0530

    added files

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      screens: {
+        xs: '0px',
+        xs1: '540px',
+        sm: '640px',
+        md: '768px',
+        md1: '860px',
+        md2: '900px',
+        md3: '950px',
+        lg: '1024px',
+        lg1: '1084px',
+        xl: '1280px',
+        xl1: '1399px',
+        '2xl': '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
+
